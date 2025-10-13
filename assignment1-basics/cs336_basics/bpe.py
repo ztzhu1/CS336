39fda6ced89e164b3a2e1c4f7226a6d09856095a
ztzhu1 <zhuzitian@zju.edu.cn>
2025-10-13 14:35:06 +0800

    try multiprocess merging

--- assignment1-basics/cs336_basics/bpe.py
+++ assignment1-basics/cs336_basics/bpe.py
@@ -14,7 +14,6 @@ from .pretokenization_example import get_chunks
 def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
     chunks = get_chunks(input_path, desired_num_chunks=100)
     freq_table = Counter()
-    pair_count = Counter()
     num_cpus = os.cpu_count()
     # pretokenization
     with Pool(processes=num_cpus) as pool:
@@ -28,44 +27,27 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
         ):
             freq_table.update(result)
 
-        # ft_chunk_size = len(freq_table) // num_cpus
-        # keys = list(freq_table)
-        # byte_pairs = Counter()
-        # args = []
-        # for i in range(num_cpus):
-        #     if i == num_cpus - 1:
-        #         subkeys = keys[i * ft_chunk_size :]
-        #     else:
-        #         subkeys = keys[i * ft_chunk_size : (i + 1) * ft_chunk_size]
-        #     args.append({key: freq_table[key] for key in subkeys})
-        # for result in tqdm(
-        #     pool.imap_unordered(pair_bytes, args), total=len(args), desc="pairing bytes"
-        # ):
-        #     byte_pairs.update(result)
-    # for arg in tqdm(args):
-    #     result = get_freq_table(arg)
-    #     freq_table.update(result)
-    byte_pairs, pair_relations = pair_bytes(freq_table, progress_bar=True)
+        byte_pairs, pair_relations = pair_bytes(freq_table, progress_bar=True)
 
-    vocab = []
-    for s in special_tokens:
-        vocab.append(s.encode("utf-8"))
-    for i in range(256):
-        vocab.append(i.to_bytes(1, "big"))
-    merges = []
-    bar = tqdm(total=vocab_size, desc="merging")
-    bar.update(len(vocab))
-    while len(vocab) < vocab_size and len(byte_pairs) > 0:
-        freq_table, byte_pairs, pair_relations, merge_key = merge(
-            freq_table, byte_pairs, pair_relations
-        )
+        vocab = []
+        for s in special_tokens:
+            vocab.append(s.encode("utf-8"))
+        for i in range(256):
+            vocab.append(i.to_bytes(1, "big"))
+        merges = []
+        bar = tqdm(total=vocab_size, desc="merging")
+        bar.update(len(vocab))
+        while len(vocab) < vocab_size and len(byte_pairs) > 0:
+            freq_table, byte_pairs, pair_relations, merge_key = merge(
+                freq_table, byte_pairs, pair_relations, pool=None
+            )
 
-        assert merge_key not in vocab
-        vocab.append(merge_key)
-        merged_pair = pair_relations[merge_key]
-        merges.append(merged_pair)
-        bar.update()
-    bar.close()
+            assert merge_key not in vocab
+            vocab.append(merge_key)
+            merged_pair = pair_relations[merge_key]
+            merges.append(merged_pair)
+            bar.update()
+        bar.close()
     vocab = dict(zip(range(len(vocab)), vocab))
     return vocab, merges
 
@@ -102,15 +84,48 @@ def pair_bytes(freq_table, progress_bar=False):
     return byte_pairs, pair_relations
 
 
-def merge(freq_table, byte_pairs, pair_relations):
+def merge(freq_table, byte_pairs, pair_relations, pool=None):
     keys = np.array(list(byte_pairs))
     values = np.array(list(byte_pairs.values()))
     indexes = np.where(values == values.max())[0]
     max_keys = keys[indexes]
     index = np.argmax(max_keys)
     merge_key = max_keys[index]
-    new_freq_table = {}
-    value = byte_pairs.pop(merge_key)
+    breakpoint()
+    merge_value = byte_pairs.pop(merge_key)
+    num_cpus = os.cpu_count()
+    chunk_size = len(freq_table) // num_cpus
+    keys = list(freq_table)
+    args = []
+    for i in range(num_cpus):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size if i < num_cpus - 1 else len(freq_table)
+        args.append(
+            (
+                {k: freq_table[k] for k in keys[start:end]},
+                pair_relations,
+                merge_key,
+                merge_value,
+            )
+        )
+    freq_table = Counter()
+    if pool is None:
+        results = []
+        for arg in args:
+            results.append(merge_part(*arg))
+    else:
+        results = pool.starmap(merge_part, args)
+    for freq_table_part, delta_byte_pairs, delta_pair_relations in results:
+        freq_table.update(freq_table_part)
+        byte_pairs.update(delta_byte_pairs)
+        pair_relations.update(delta_pair_relations)
+    return freq_table, byte_pairs, pair_relations, merge_key
+
+
+def merge_part(freq_table, pair_relations, merge_key, merge_value):
+    new_freq_table = Counter()
+    delta_pair_relations = {}
+    delta_byte_pairs = Counter()
     for key in list(freq_table):
         new_key = []
         skip_next = False
@@ -129,22 +144,22 @@ def merge(freq_table, byte_pairs, pair_relations):
                     if new_pair in pair_relations:
                         assert pair_relations[new_pair] == (key[i - 1], pair)
                     else:
-                        pair_relations[new_pair] = (key[i - 1], pair)
-                    byte_pairs[new_pair] += value
+                        delta_pair_relations[new_pair] = (key[i - 1], pair)
+                    delta_byte_pairs[new_pair] += merge_value
                 if i < len(key) - 2:
                     new_pair = pair + key[i + 2]
                     if new_pair in pair_relations:
                         assert pair_relations[new_pair] == (pair, key[i + 2])
                     else:
-                        pair_relations[new_pair] = (pair, key[i + 2])
-                    byte_pairs[new_pair] += value
+                        delta_pair_relations[new_pair] = (pair, key[i + 2])
+                    delta_byte_pairs[new_pair] += merge_value
 
                 skip_next = True
             else:
                 new_key.append(key[i])
                 skip_next = False
         new_freq_table[tuple(new_key)] = freq_table[key]
-    return new_freq_table, byte_pairs, pair_relations, merge_key
+    return new_freq_table, delta_byte_pairs, delta_pair_relations
 
 
 def to_bytes_array(word: bytes):
